4a40e09371259c75ace9b37b86279263f73fc9f3
mcb2003 <mikey@blindcomputing.org>
2021-08-18 20:34:09 +0100

    Implement Transfer::transferred() and Transfer::finish()
    transferred() returns the number of bytes transferred thus far, finish()
    joins the inner thread and can be used to handle IO errors and retrieve
    the reader and writer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use std::{
     io::{self, prelude::*},
-    sync::{Arc, atomic::{AtomicU64, Ordering}},
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
     thread,
 };
 
@@ -8,26 +11,43 @@ use progress_streams::ProgressReader;
 
 pub struct Transfer<R, W>
 where
-R: Read + Send + 'static,
-W: Write + Send + 'static {
+    R: Read + Send + 'static,
+    W: Write + Send + 'static,
+{
     transferred: Arc<AtomicU64>,
     handle: thread::JoinHandle<io::Result<(R, W)>>,
 }
 
 impl<R, W> Transfer<R, W>
 where
-R: Read + Send + 'static,
-W: Write + Send + 'static {
+    R: Read + Send + 'static,
+    W: Write + Send + 'static,
+{
     pub fn new(reader: R, mut writer: W) -> Self {
         let transferred = Arc::new(AtomicU64::new(0));
         let transferred_clone = Arc::clone(&transferred);
         let handle = thread::spawn(move || -> io::Result<(R, W)> {
-                                   let mut reader = ProgressReader::new(reader, |bytes| {
-                                                                    transferred_clone.fetch_add(bytes as u64, Ordering::Release);
-                                   });
-                                   io::copy(&mut reader, &mut writer)?;
-                                   Ok((reader.into_inner(), writer))
+            let mut reader = ProgressReader::new(reader, |bytes| {
+                // If someone would like to confirm the correctness of the ordering guarantees, that would
+                // be much appreciated.
+                transferred_clone.fetch_add(bytes as u64, Ordering::Release);
+            });
+            io::copy(&mut reader, &mut writer)?;
+            Ok((reader.into_inner(), writer))
         });
-        Self { transferred, handle }
+        Self {
+            transferred,
+            handle,
+        }
+    }
+
+    pub fn finish(self) -> io::Result<(R, W)> {
+        self.handle.join().unwrap()
+    }
+
+    pub fn transferred(&self) -> u64 {
+        // If someone would like to confirm the correctness of the ordering guarantees, that would
+        // be much appreciated.
+        self.transferred.load(Ordering::Acquire)
     }
 }
