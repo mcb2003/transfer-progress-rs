5a1f7b8e8fb728bc20b119c7021475928e8760d5
mcb2003 <mikey@blindcomputing.org>
2021-08-19 11:11:15 +0100

    Add documentation and examples (doc-tests).

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@ struct TransferState {
     complete: AtomicBool,
 }
 
+/// Monitors the progress of a transfer from a [reader][Read] to a [writer][Write].
 pub struct Transfer<R, W>
 where
     R: Read + Send + 'static,
@@ -35,6 +36,16 @@ where
     R: Read + Send + 'static,
     W: Write + Send + 'static,
 {
+    /// Creates and starts a new `Transfer`.
+    /// # Example
+    /// ```no_run
+    /// use transfer_progress::Transfer;
+    /// use std::fs::File;
+    /// let reader = File::open("file1.txt")?;
+    /// let writer = File::create("file2.txt")?;
+    /// let transfer = Transfer::new(reader, writer);
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn new(reader: R, mut writer: W) -> Self {
         let state = Arc::new(TransferState::default());
         let state_clone = Arc::clone(&state);
@@ -58,26 +69,98 @@ where
         }
     }
 
+    /// Consumes the `Transfer`, blocking until the transfer is complete.
+    ///
+    /// If the transfer was successful, returns `Ok(reader, writer)`, otherwise returns
+    /// the error.
+    ///
+    /// If the transfer is already complete, returns immediately.
+    /// # Example
+    /// ```no_run
+    /// use transfer_progress::Transfer;
+    /// use std::fs::File;
+    /// let reader = File::open("file1.txt")?;
+    /// let writer = File::create("file2.txt")?;
+    /// let transfer = Transfer::new(reader, writer);
+    /// let (reader, writer) = transfer.finish()?;
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn finish(self) -> io::Result<(R, W)> {
         self.handle.join().unwrap()
     }
 
+    /// Tests if the transfer is complete
+    /// # Example
+    /// ```no_run
+    /// use transfer_progress::Transfer;
+    /// use std::fs::File;
+    /// let reader = File::open("file1.txt")?;
+    /// let writer = File::create("file2.txt")?;
+    /// let transfer = Transfer::new(reader, writer);
+    /// while !transfer.is_complete() {
+    /// println!("Not complete yet");
+    /// std::thread::sleep(std::time::Duration::from_secs(1));
+    /// }
+    /// println!("Complete!");
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn is_complete(&self) -> bool {
         // If someone would like to confirm the correctness of the ordering guarantees, that would
         // be much appreciated.
         self.state.complete.load(Ordering::Acquire)
     }
 
+    /// Returns the number of bytes transferred thus far between the reader and the writer.
+    /// # Example
+    /// ```no_run
+    /// use transfer_progress::Transfer;
+    /// use std::fs::File;
+    /// let reader = File::open("file1.txt")?;
+    /// let writer = File::create("file2.txt")?;
+    /// let transfer = Transfer::new(reader, writer);
+    /// while !transfer.is_complete() {
+    /// println!("{} bytes transferred so far", transfer.transferred());
+    /// std::thread::sleep(std::time::Duration::from_secs(1));
+    /// }
+    /// println!("Complete!");
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn transferred(&self) -> u64 {
         // If someone would like to confirm the correctness of the ordering guarantees, that would
         // be much appreciated.
         self.state.transferred.load(Ordering::Acquire)
     }
 
+    /// Returns the elapsed time since the transfer started.
+    /// # Example
+    /// ```no_run
+    /// use transfer_progress::Transfer;
+    /// use std::fs::File;
+    /// let reader = File::open("file1.txt")?;
+    /// let writer = File::create("file2.txt")?;
+    /// let transfer = Transfer::new(reader, writer);
+    /// while !transfer.is_complete() {}
+    /// println!("Transfer took {:?}", transfer.running_time());
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn running_time(&self) -> Duration {
         self.start_time.elapsed()
     }
 
+    /// Returns the average speed, in bytes per second, of the transfer.
+    /// # Example
+    /// ```no_run
+    /// use transfer_progress::Transfer;
+    /// use std::fs::File;
+    /// let reader = File::open("file1.txt")?;
+    /// let writer = File::create("file2.txt")?;
+    /// let transfer = Transfer::new(reader, writer);
+    /// while !transfer.is_complete() {
+    /// println!("{}B/s", transfer.speed());
+    /// std::thread::sleep(std::time::Duration::from_secs(1));
+    /// }
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn speed(&self) -> u64 {
         (self.transferred() as f64 / self.running_time().as_secs_f64()).round() as u64
     }
@@ -117,6 +200,7 @@ where
     }
 }
 
+/// Monitors the progress of a transfer with a known size.
 pub struct SizedTransfer<R, W>
 where
     R: Read + Send + 'static,
@@ -131,6 +215,17 @@ where
     R: Read + Send + 'static,
     W: Write + Send + 'static,
 {
+    /// Creates and starts a new `SizedTransfer`.
+    /// # Example
+    /// ```no_run
+    /// use transfer_progress::SizedTransfer;
+    /// use std::fs::File;
+    /// use std::io::Read;
+    /// let reader = File::open("file1.txt")?.take(1024); // Bytes
+    /// let writer = File::create("file2.txt")?;
+    /// let transfer = SizedTransfer::new(reader, writer, 1024);
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn new(reader: R, writer: W, size: u64) -> Self {
         Self {
             inner: Transfer::new(reader, writer),
@@ -138,14 +233,67 @@ where
         }
     }
 
+    /// Consumes the `SizedTransfer`, blocking until the transfer is complete.
+    ///
+    /// If the transfer was successful, returns `Ok(reader, writer)`, otherwise returns
+    /// the error.
+    ///
+    /// If the transfer is already complete, returns immediately.
+    /// # Example
+    /// ```no_run
+    /// use transfer_progress::SizedTransfer;
+    /// use std::fs::File;
+    /// use std::io::Read;
+    /// let reader = File::open("file1.txt")?.take(1024); // Bytes
+    /// let writer = File::create("file2.txt")?;
+    /// let transfer = SizedTransfer::new(reader, writer, 1024);
+    /// let (reader, writer) = transfer.finish()?;
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn finish(self) -> io::Result<(R, W)> {
         self.inner.finish()
     }
 
+    /// Returns a fraction between 0.0 and 1.0 representing the state of the transfer.
+    /// # Example
+    /// ```no_run
+    /// use transfer_progress::SizedTransfer;
+    /// use std::fs::File;
+    /// use std::io::Read;
+    /// let reader = File::open("file1.txt")?.take(1024); // Bytes
+    /// let writer = File::create("file2.txt")?;
+    /// let transfer = SizedTransfer::new(reader, writer, 1024);
+    /// while !transfer.is_complete() {
+    /// println!("Transfer is {:.0}% complete", transfer.fraction_transferred() * 100.0);
+    /// std::thread::sleep(std::time::Duration::from_secs(1));
+    /// }
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn fraction_transferred(&self) -> f64 {
         self.transferred() as f64 / self.size as f64
     }
 
+    /// Returns the approximate remaining time until this transfer completes. Returns `None` if
+    /// this cannot be calculated (I.E. no bytes have been transferred yet, so a speed cannot be
+    /// determined).
+    /// # Example
+    /// ```no_run
+    /// use transfer_progress::SizedTransfer;
+    /// use std::fs::File;
+    /// use std::io::Read;
+    /// let reader = File::open("file1.txt")?.take(1024); // Bytes
+    /// let writer = File::create("file2.txt")?;
+    /// let transfer = SizedTransfer::new(reader, writer, 1024);
+    /// while !transfer.is_complete() {
+    /// if let Some(eta) = transfer.eta() {
+    /// println!("Transfer will complete in approximately {:?}", eta);
+    /// } else {
+    /// println!("Transfer completion time is unknown");
+    /// }
+    /// std::thread::sleep(std::time::Duration::from_secs(1));
+    /// }
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn eta(&self) -> Option<Duration> {
         // Cache this so we don't have to perform an atomic access twice
         let transferred = self.inner.transferred();
